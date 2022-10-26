7f82042da18067f6cf035dc2f4d697443e827864
davblack <davidfekete51@gmail.com>
2022-10-26 16:43:29 +0200

    api inaskodas v2

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -4,44 +4,65 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CheeseListingRepository;
+use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 
 #[ORM\Entity(repositoryClass: CheeseListingRepository::class)]
 #[ApiResource(
-    collectionOperations:[
-        'get' => [
-            'shortName' => 'cheeses'
-        ],
+    collectionOperations: [
+        'get',
         'post'
     ],
-    itemOperations: ['get', 'put'],
-    shortName: 'cheeses'
+    itemOperations: [
+        'get',
+        'put'
+    ],
+    shortName: 'cheeses',
+    denormalizationContext: [
+        'groups' => [self::CHEESE_LISTING_WRITE]
+    ],
+    normalizationContext: [
+        'groups' => [self::CHEESE_LISTING_READ]
+    ],
 )]
-
 class CheeseListing
 {
+    public const CHEESE_LISTING_READ = "cheese_listing:read";
+    public const CHEESE_LISTING_WRITE = "cheese_listing:write";
+
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups([self::CHEESE_LISTING_READ, self::CHEESE_LISTING_WRITE])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups([self::CHEESE_LISTING_READ])]
     private ?string $description = null;
 
-
     #[ORM\Column]
+    #[Groups([self::CHEESE_LISTING_READ, self::CHEESE_LISTING_WRITE])]
     private ?int $price = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
     private ?\DateTimeInterface $cretedAt = null;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    private ?bool $isPublished = false;
+
+    public function __construct(string $title)
+    {
+        $this->cretedAt = new \DateTimeImmutable();
+        $this->title = $title;
+    }
 
     public function getId(): ?int
     {
@@ -52,14 +73,14 @@ class CheeseListing
     {
         return $this->title;
     }
-
+/*
     public function setTitle(string $title): self
     {
         $this->title = $title;
 
         return $this;
     }
-
+*/
     public function getDescription(): ?string
     {
         return $this->description;
@@ -68,6 +89,15 @@ class CheeseListing
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    #[Groups([self::CHEESE_LISTING_WRITE])]
+    #[SerializedName('description')]
+    public function setTextDescription(string $description): self
+    {
+        $this->description = nl2br($description);
 
         return $this;
     }
@@ -89,11 +119,9 @@ class CheeseListing
         return $this->cretedAt;
     }
 
-    public function setCretedAt(\DateTimeInterface $cretedAt): self
+    public function getCreatedAtAgo(): string
     {
-        $this->cretedAt = $cretedAt;
-
-        return $this;
+        return Carbon::instance($this->getCretedAt())->diffForHumans();
     }
 
     public function isIsPublished(): ?bool
@@ -107,4 +135,5 @@ class CheeseListing
 
         return $this;
     }
+
 }
